be3d120d9500792c76f3bd2a1a1b1298003f2a1f
Devang <dbhagwat512@gmail.com>
2026-09-19 13:08:12 +0530

    Modify the testcontainer config for MongoDB
    After switching from PostgreSQL to MongoDB, this commit changes the
    TestContainersConfiguration to configure a MongoDB container instead of
    a PostgreSQL DB container.

--- src/test/java/com/training/urlshortener/TestcontainersConfiguration.java
+++ src/test/java/com/training/urlshortener/TestcontainersConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.mongodb.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,8 +12,8 @@ class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    MongoDBContainer postgresContainer() {
+        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
     }
 
     @Bean
