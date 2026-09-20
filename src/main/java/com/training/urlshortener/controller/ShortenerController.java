3d1eaa1e86456591c5f97c09891cac5d38611250
Devang <dbhagwat512@gmail.com>
2026-09-20 12:57:42 +0530

    Define the URL registration endpoint

--- src/main/java/com/training/urlshortener/controller/ShortenerController.java
+++ src/main/java/com/training/urlshortener/controller/ShortenerController.java
@@ -1,27 +1,33 @@
 package com.training.urlshortener.controller;
 
+import com.training.urlshortener.dto.UrlRequest;
+import com.training.urlshortener.dto.UrlResponse;
 import com.training.urlshortener.service.UrlService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/v1/api")
 public class ShortenerController {
 
     private final UrlService urlService;
 
-    @Cacheable(value = "urls", key = "#id")
     @GetMapping(path = "/{id}")
     ResponseEntity<Void> redirect(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(urlService.getLongUrl(id))
                 .build();
+    }
+
+    @PostMapping(path = "/v1/api/shorten")
+    ResponseEntity<UrlResponse> getShortUrl(HttpServletRequest request, @RequestBody UrlRequest urlRequest) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(urlService.registerUrl(request, urlRequest.toUri()));
     }
 }
