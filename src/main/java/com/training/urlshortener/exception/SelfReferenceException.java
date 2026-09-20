c647965cff961bfcdf2d456ba8fe5dac9eb7a162
Devang <dbhagwat512@gmail.com>
2026-09-20 12:50:46 +0530

    Define an exception for self referencing URLs

--- src/main/java/com/training/urlshortener/exception/SelfReferenceException.java
+++ src/main/java/com/training/urlshortener/exception/SelfReferenceException.java
@@ -1,4 +1,9 @@
 package com.training.urlshortener.exception;
 
-public class SelfReferenceException {
+public class SelfReferenceException extends RuntimeException {
+    private static final String MESSAGE = "The given URL references to this app";
+
+    public SelfReferenceException() {
+        super(MESSAGE);
+    }
 }
