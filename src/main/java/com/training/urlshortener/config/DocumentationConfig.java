33cf38ed9b5c61c0bd0ee7a402640f857207fade
Devang <dbhagwat512@gmail.com>
2026-09-20 20:33:43 +0530

    Add simple Documentation configuration

---
+++ src/main/java/com/training/urlshortener/config/DocumentationConfig.java
@@ -0,0 +1,34 @@
+package com.training.urlshortener.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DocumentationConfig {
+
+    private static final String INSTRUCTIONS = """
+            ### URL shortening service
+            
+            Use the POST /v1/api/shorten API to shorten a URL.
+            This app uses Nano IDs to generate a cryptographically random ID to your long URL!
+            
+            Accessing /{id} URL will redirect to the URL registered against it.
+            """;
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        var contactInfo = new Contact()
+                .name("Devang Hemant Bhagwat")
+                .email("dbhagwat512@gmail.com");
+        var information = new Info()
+                .title("URL Shortener")
+                .version("1.0")
+                .description(INSTRUCTIONS)
+                .contact(contactInfo);
+        return new OpenAPI()
+                .info(information);
+    }
+}
