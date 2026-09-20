23aa2a060fa7c97e5c12de25178901a931547c69
Devang <dbhagwat512@gmail.com>
2026-09-20 20:20:08 +0530

    Add an exception handler for possible exceptions

---
+++ src/main/java/com/training/urlshortener/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,29 @@
+package com.training.urlshortener.advice;
+
+import com.training.urlshortener.dto.ApiErrorResponse;
+import com.training.urlshortener.exception.MissingEntryException;
+import com.training.urlshortener.factory.ErrorMessageFactory;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            HandlerMethodValidationException.class,
+            MissingEntryException.class
+    })
+    ResponseEntity<ApiErrorResponse> handleBadRequest(Exception ex, HttpServletRequest request) {
+        return ErrorMessageFactory.getApiErrorResponseEntity(ex, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<ApiErrorResponse> handleGenericExceptions(Exception ex, HttpServletRequest request) {
+        return ErrorMessageFactory.getApiErrorResponseEntity(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+}
